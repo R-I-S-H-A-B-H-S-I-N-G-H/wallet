516976524e500d442d8c7c892c7c178872fb27a2
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-08-04 23:40:00 +0530

    chnages in cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import walletRouter from "./routes/wallet.route";
-
+import cors from 'cors'
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.json())
 app.use("/wallet", walletRouter);
 app.get("*", (req, res) => { 
