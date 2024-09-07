e6eb3a13378e4c02cf913a1eeb568b39257645a6
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-09-08 00:36:07 +0530

    chnages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import styleX from "vite-plugin-stylex";
 import react from "@vitejs/plugin-react-swc";
-import { stylex } from "vite-plugin-stylex-dev";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), styleX(), stylex()],
+	plugins: [react()],
 });
